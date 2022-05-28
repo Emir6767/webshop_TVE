4123c5eaa43902f99545ae4148f7f8bc75644c86
Emir6767 <Emir.Akkaya@student.Reutlingen-University.de>
2022-05-28 13:34:30 +0200

    probeeee

--- php/Artikelseite.php
+++ php/Artikelseite.php
@@ -76,7 +76,7 @@ $result3 = $db->query($smokebox);
       <hr>
   
       <h3>Smokeboxen</h3>
-          // HALLO 
+          // HALL
       <hr>
       <div class="row">
         <?php while($row = $result3->fetch()):?>
